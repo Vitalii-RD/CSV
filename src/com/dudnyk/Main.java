900837632e08ecaa55ae322d9443d7e3691f59fe
Vitaliy5 <dydvitrom@ukr.net>
2020-11-26 22:34:02 +0200

    update main class and add vsc.xml

--- src/com/dudnyk/Main.java
+++ src/com/dudnyk/Main.java
@@ -1,8 +1,17 @@
 package com.dudnyk;
 
-public class Main {
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args){
+        String csvFile = "C:\\Users\\DVR\\Desktop\\acme_worksheet.csv";
+        CSV csv = new CSV();
+
+        List<Employee> employees = csv.getEmployees(csvFile);
+        Collections.sort(employees, Comparator.comparing(Employee::getDate));
+
+        csv.generateCSVFile("file", employees);
     }
 }
